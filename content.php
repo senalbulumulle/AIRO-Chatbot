d642965de4f63be26ba4a334da3b50ad81b165e9
Senal Bulumulle <senal.bulumulle@gmail.com>
2021-05-23 09:24:41 -0400

    Line 2-3: Removed Comments
    This file is located in "AIRO-Chatbot/content.php"

--- content.php
+++ content.php
@@ -1,4 +1,3 @@
 <div class="FOIL-Layout color-is-blue">
-    <!-- This is where the Rasa API will go -->
-    <!-- We need to get rasa to work using the python dependencies for rasa. -->
+    
 </div>
